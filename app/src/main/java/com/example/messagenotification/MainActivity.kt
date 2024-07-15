09d2396a08d2f10bc6e312c5867c7a97bd566080
Bharath Parivallal <bharath.parivallal@digivalsolutions.com>
2024-07-15 10:33:09 +0530

    first commit

---
+++ app/src/main/java/com/example/messagenotification/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.messagenotification
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import com.example.messagenotification.View.FirebaseMessagingScreen
+import com.example.messagenotification.ui.theme.MessageNotificationTheme
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful){
+                Log.w("Failed", "fetching token registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+            Log.d("Token", token.toString())
+            Toast.makeText(baseContext,token.toString(), Toast.LENGTH_SHORT).show()
+        })
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessageNotificationTheme {
+                FirebaseMessagingScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/messagenotification/View/FirebaseMessagingScreen.kt
@@ -0,0 +1,69 @@
+package com.example.messagenotification.View
+
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.messagenotification.View.Component.FirebaseMessagingNotificationPermissionDialog
+import com.example.messagenotification.ui.theme.MessageNotificationTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun FirebaseMessagingScreen() {
+
+    val showNotificationDialog = remember { mutableStateOf(false) }
+    val notificationPermissionState = rememberPermissionState(
+        permission = Manifest.permission.POST_NOTIFICATIONS
+    )
+    if (showNotificationDialog.value) FirebaseMessagingNotificationPermissionDialog(
+        showNotificationDialog = showNotificationDialog,
+        notificationPermissionState = notificationPermissionState
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        if (notificationPermissionState.status.isGranted ||
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU
+            ) {
+                Firebase.messaging.subscribeToTopic("Tutorial")
+        } else showNotificationDialog.value = true
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello, main screen", color = Color.Black, fontSize = 20.sp, fontStyle = FontStyle.Italic)
+    }
+}
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@Preview
+@Composable
+fun MainScreenPreview(){
+    MessageNotificationTheme {
+        FirebaseMessagingScreen()
+    }
+}
